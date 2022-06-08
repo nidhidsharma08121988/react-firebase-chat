fa7994ecaae944f07635d10aab9cb9d647af3fcf
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-09 00:32:32 +0530

    Fix and update: 1. Fix warnings *update index.js to use createRoot from react-dom/client *update manifest.json to remove react logo warning 2.Update UI *update SignUpPage to include a sign up form * update SignUpPage to include option to login using google

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,22 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
 
-const SignUpPage = ({ auth, email, password }) => {
-  const signInWithGoogle = () => {
-    createUserWithEmailAndPassword(auth, email, password)
+const SignUpPage = ({ auth }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const signUp = async () => {
+    console.log('in signup')
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+      console.log('User created', userCredential)
+    } catch (error) {
+      console.log('In error')
+    }
   }
   return (
-    <form>
-      <label htmlFor='email'>
-        <input id='email' type='email' />
-      </label>
-      <label for='password'>
-        <input id='password' type='password' />
-      </label>
-      <button type='submit' onClick={signInWithGoogle}>
-        Sign up
-      </button>
-    </form>
+    <div>
+      <form>
+        <label htmlFor='email'>
+          <input
+            id='email'
+            type='email'
+            placeholder='Email Address'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label htmlFor='password'>
+          <input
+            id='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            minLength={6}
+            required
+          />
+        </label>
+        <button type='submit' onClick={signUp}>
+          Sign up
+        </button>
+      </form>
+      <div>
+        <div>or</div>
+        <button>SignUp using google</button>
+      </div>
+    </div>
   )
 }
 
