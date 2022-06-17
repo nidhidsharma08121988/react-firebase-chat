e015add53a001e34147e674dbc92f09237b83837
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-17 07:35:13 +0530

    style: update header css module

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,9 +1,10 @@
-// import { auth } from '../FirebaseModule.js'
+import { auth } from '../FirebaseModule.js'
+import styles from './ChatRoom.module.css'
 const ChatRoom = ({ user }) => {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className={styles.chatRoomContainer}>
+      <div className={styles.chatNav}>Chat nav</div>
+      <div className={styles.roomArea}>Room area</div>
     </div>
   )
 }
