2ba98cb1fbe31d3806d28a6eb8a777542391a192
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-19 14:04:48 +0530

    style: Room list

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,51 +1,11 @@
-import { auth } from '../FirebaseModule.js'
 import styles from './ChatRoom.module.css'
-const ChatRoom = ({ user }) => {
-  const rooms = {
-    rr1: {
-      title: 'Wedding Dhamaka',
-      admins: ['namrata@yahoo.com'],
-      participants: ['namrata@yahoo.com', 'puja@yahoo.com', 'nidhi@yahoo.com'],
-    },
-    rr2: {
-      title: 'DDS Family',
-      admins: ['dd@yahoo.com', 'meeta@yahoo.com'],
-      participants: [
-        'dd@yahoo.com',
-        'meeta@yahoo.com',
-        'puja@yahoo.com',
-        'nidhi@yahoo.com',
-        'pankaj@yahoo.com',
-        'manish@yahoo.com',
-      ],
-    },
-    rr3: {
-      title: 'BFF',
-      admins: [
-        'nidhi@yahoo.com',
-        'manisha@yahoo.com',
-        'dhwani@yahoo.com',
-        'swaleha@yahoo.com',
-      ],
-      participants: [
-        'nidhi@yahoo.com',
-        'manisha@yahoo.com',
-        'dhwani@yahoo.com',
-        'swaleha@yahoo.com',
-      ],
-    },
-  }
-  const roomIds = Object.keys(rooms)
-  const RoomsList = (
-    <ul>
-      {roomIds.map(roomId => (
-        <li key={roomId}>{rooms[roomId].title}</li>
-      ))}
-    </ul>
-  )
+import Rooms from './Rooms'
+
+const ChatRoom = () => {
+  const rooms = Rooms()
   return (
     <div className={styles.chatRoomContainer}>
-      <div className={styles.chatRoomsNav}>{RoomsList}</div>
+      <div className={styles.chatRoomsNav}>{rooms}</div>
       <div className={styles.chatArea}>Chat area</div>
     </div>
   )
---
+++ src/components/Rooms.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from './Rooms.module.css'
+
+const Rooms = () => {
+  const rooms = {
+    rr1: {
+      title: 'Wedding Dhamaka',
+      admins: ['namrata@yahoo.com'],
+      participants: ['namrata@yahoo.com', 'puja@yahoo.com', 'nidhi@yahoo.com'],
+    },
+    rr2: {
+      title: 'DDS Family',
+      admins: ['dd@yahoo.com', 'meeta@yahoo.com'],
+      participants: [
+        'dd@yahoo.com',
+        'meeta@yahoo.com',
+        'puja@yahoo.com',
+        'nidhi@yahoo.com',
+        'pankaj@yahoo.com',
+        'manish@yahoo.com',
+      ],
+    },
+    rr3: {
+      title: 'BFF',
+      admins: [
+        'nidhi@yahoo.com',
+        'manisha@yahoo.com',
+        'dhwani@yahoo.com',
+        'swaleha@yahoo.com',
+      ],
+      participants: [
+        'nidhi@yahoo.com',
+        'manisha@yahoo.com',
+        'dhwani@yahoo.com',
+        'swaleha@yahoo.com',
+      ],
+    },
+  }
+  const roomIds = Object.keys(rooms)
+  const RoomsList = (
+    <ul className={styles.rooms}>
+      {roomIds.map(roomId => (
+        <li key={roomId} className={styles.room}>
+          {rooms[roomId].title}
+        </li>
+      ))}
+    </ul>
+  )
+  return RoomsList
+}
+
+export default Rooms
