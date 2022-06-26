1061bf10a8032cb2b7536147dadc8a6340a62003
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-26 18:46:24 +0530

    fix: when user logs out he cannot see home page. *use  useNavigate hook to route to home page

--- src/components/SignOutButton.js
+++ src/components/SignOutButton.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { signOut } from 'firebase/auth'
 import { auth } from '../backend/FirebaseModule'
 import styles from './SignOutButton.module.css'
 
 const SignOutButton = () => {
+  const navigate = useNavigate()
+
   const handleClick = async e => {
     try {
       await signOut(auth)
+      navigate('/')
     } catch (error) {
       console.log('in error', error.message)
     }
