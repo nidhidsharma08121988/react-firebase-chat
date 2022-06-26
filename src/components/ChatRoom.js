9470228afd6a478da9e711b702db6254c41fce18
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-26 18:49:27 +0530

    fix: unauthenticated users must see a warning about logging in if they try to access a route that requires authenticated user

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -4,14 +4,18 @@ import { ChatContext } from '../store/ChatState'
 import { useContext } from 'react'
 
 const ChatRoom = () => {
-  const { selectedRoom } = useContext(ChatContext)
+  const { user, selectedRoom } = useContext(ChatContext)
 
-  return (
+  return user ? (
     <div className={styles.chatRoomContainer}>
       <div className={styles.chatRoomsNav}>
         <Rooms />
       </div>
       <div className={styles.chatArea}>{<h4>{selectedRoom.title}</h4>}</div>
+    </div>
+  ) : (
+    <div className={styles.chatRoomContainer}>
+      <h3>Please login to access this url</h3>
     </div>
   )
 }
