d8567d115550f4178221659606e11ce4ebcf9d76
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-26 19:00:45 +0530

    fix and style: when unauthenticated user access chatroom they also must see the login and back button * styling of buttons and container

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -2,6 +2,7 @@ import styles from './ChatRoom.module.css'
 import Rooms from './Rooms'
 import { ChatContext } from '../store/ChatState'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 
 const ChatRoom = () => {
   const { user, selectedRoom } = useContext(ChatContext)
@@ -14,8 +15,16 @@ const ChatRoom = () => {
       <div className={styles.chatArea}>{<h4>{selectedRoom.title}</h4>}</div>
     </div>
   ) : (
-    <div className={styles.chatRoomContainer}>
+    <div className={styles.warningContainer}>
       <h3>Please login to access this url</h3>
+      <div>
+        <Link to='/'>
+          <button className={styles.submit}>Back</button>
+        </Link>
+        <Link to='/signIn'>
+          <button className={styles.submit}>Log In</button>
+        </Link>
+      </div>
     </div>
   )
 }
