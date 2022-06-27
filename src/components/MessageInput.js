44512a3dca46f67fbf1834a22e2734c6be8d5a32
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-27 09:54:57 +0530

    refactor: component to have an input box and button

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const MessageInput = () => {
-  return <div>MessageInput</div>
+  return (
+    <div>
+      <input />
+      <button>Send message</button>
+    </div>
+  )
 }
 
 export default MessageInput
