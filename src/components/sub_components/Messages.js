989cc06f60a239222623098735cadd572728e060
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-27 13:33:05 +0530

    style: all about styling chat room, messages and send form(intermediate)

--- src/components/sub_components/Messages.js
+++ src/components/sub_components/Messages.js
@@ -5,7 +5,12 @@ import { ChatContext } from '../../store/ChatState'
 import styles from './Messages.module.css'
 
 const Message = ({ message }) => {
-  return <li>{message.text}</li>
+  return (
+    <li className={styles.messageContainer}>
+      <div>{message.authorEmail}</div>
+      <div>{message.text}</div>
+    </li>
+  )
 }
 const Messages = () => {
   const { selectedRoom, getMessagesOfSelectedRoom, messages } =
