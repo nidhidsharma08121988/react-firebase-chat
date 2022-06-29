0ea11fe5f373e318b1b366b258490b4c55daae1e
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-29 15:29:21 +0530

    update: sendMessageForm

--- src/components/sub_components/SendMessageForm.js
+++ src/components/sub_components/SendMessageForm.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SendMessageForm.module.css'
 const SendMessageForm = () => {
+  const [text, setText] = useState('')
+
+  const sendMessage = e => {
+    e.preventDefault()
+  }
   return (
-    <div className={styles.messageFormContainer}>
-      <input className={styles.messageInput} />
-      <div className={styles.submit}>
-        <i class='fa-solid fa-greater-than'></i>
-      </div>
-    </div>
+    <form className={styles.messageFormContainer} onSubmit={sendMessage}>
+      <input
+        type='text'
+        value={text}
+        onChange={e => setText(e.target.value)}
+        placeholder='Message...'
+        className={styles.messageInput}
+      />
+      <button className={styles.submit} type='submit'>
+        <i className='fa fa-solid fa-paper-plane'></i>
+      </button>
+    </form>
   )
 }
 
