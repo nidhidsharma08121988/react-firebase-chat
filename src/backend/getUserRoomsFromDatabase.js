3c51e0423c8e2027a987d84b45217d919bf84040
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-29 18:37:53 +0530

    fix: add creatorEmail property to room object

--- src/backend/getUserRoomsFromDatabase.js
+++ src/backend/getUserRoomsFromDatabase.js
@@ -1,10 +1,12 @@
 const rooms = {
   rr1: {
     title: 'Wedding Dhamaka',
+    creatorEmail: 'nidhi@yahoo.com',
     participants: ['namrata@yahoo.com', 'puja@yahoo.com', 'nidhi@yahoo.com'],
   },
   rr2: {
     title: 'DDS Family',
+    creatorEmail: 'nidhi@yahoo.com',
     participants: [
       'dd@yahoo.com',
       'meeta@yahoo.com',
@@ -14,6 +16,7 @@ const rooms = {
   },
   rr3: {
     title: 'BFF',
+    creatorEmail: 'nidhi@yahoo.com',
     participants: ['nidhi@yahoo.com', 'manisha@yahoo.com', 'dhwani@yahoo.com'],
   },
 }
