05f94db1116ea12c4ac71f5ad2ffbc84a3576136
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-29 17:36:07 +0530

    update: sendMessageForm to make it ready for pushed into messages

--- src/components/sub_components/SendMessageForm.js
+++ src/components/sub_components/SendMessageForm.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'
+import { useContext } from 'react'
 import styles from './SendMessageForm.module.css'
+import { ChatContext } from './../../store/ChatState'
 const SendMessageForm = () => {
   const [text, setText] = useState('')
+  const { selectedRoom, user } = useContext(ChatContext)
 
   const sendMessage = e => {
     e.preventDefault()
+    const message = {
+      text: text,
+      roomId: selectedRoom.id,
+      authorEmail: user.email,
+      created: Date.now(),
+    }
+    console.log(message)
+    setText('')
   }
   return (
     <form className={styles.messageFormContainer} onSubmit={sendMessage}>
@@ -14,6 +25,7 @@ const SendMessageForm = () => {
         onChange={e => setText(e.target.value)}
         placeholder='Message...'
         className={styles.messageInput}
+        required
       />
       <button className={styles.submit} type='submit'>
         <i className='fa fa-solid fa-paper-plane'></i>
