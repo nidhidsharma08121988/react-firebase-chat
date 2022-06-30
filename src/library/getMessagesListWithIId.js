38076b44dff10f8d24f1fb74698a2e53d42907ff
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-06-30 10:46:35 +0530

    fix: the messages keys became faulty when we tried to add two ids into it. Remove code that adds keys to already added ids

--- src/library/getMessagesListWithIId.js
+++ src/library/getMessagesListWithIId.js
@@ -2,11 +2,6 @@ import { getAllMessagesWithIdFromDatabase } from './../backend/FirebaseModule'
 
 export const getMessageListWithIds = async () => {
   const messages = await getAllMessagesWithIdFromDatabase()
-  const messageIds = Object.keys(messages)
-  const allMessagesList = messageIds.map(messageId => ({
-    ...messages[messageId],
-    id: messageId,
-  }))
 
-  return allMessagesList
+  return messages
 }
