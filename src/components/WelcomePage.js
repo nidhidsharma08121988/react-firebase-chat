0674fe95316ec41a12141bff59866b0ab8f8c6f5
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-04 09:03:49 +0530

    fix: when user is logged in then home page should route itself to Chat Room route

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,26 +1,27 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { ChatContext } from '../store/ChatState'
 import SignUpPage from './SignUpPage'
 import styles from './WelcomePage.module.css'
 
 const WelcomePage = () => {
   const { isLoggedIn } = useContext(ChatContext)
-
-  return (
-    !isLoggedIn && (
-      <div className={styles.welcomeImageSignUpContainer}>
-        <div className={styles.welcomeImage}>
-          <img
-            src={require('./MyChitChat.gif')}
-            alt='welcome'
-            className={styles.myWelcomeImage}
-          />
-        </div>
-        <div className={styles.formContainer}>
-          <SignUpPage />
-        </div>
+  const navigate = useNavigate()
+  return !isLoggedIn ? (
+    <div className={styles.welcomeImageSignUpContainer}>
+      <div className={styles.welcomeImage}>
+        <img
+          src={require('./MyChitChat.gif')}
+          alt='welcome'
+          className={styles.myWelcomeImage}
+        />
       </div>
-    )
+      <div className={styles.formContainer}>
+        <SignUpPage />
+      </div>
+    </div>
+  ) : (
+    navigate('/chatRoom')
   )
 }
 
