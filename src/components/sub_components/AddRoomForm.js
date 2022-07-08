39220eeb6899a7c7ed712474fecef531bec3c76a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-08 15:46:50 +0530

    update: AddRoomForm to call api and update chatState to add new room

--- src/components/sub_components/AddRoomForm.js
+++ src/components/sub_components/AddRoomForm.js
@@ -1,11 +1,29 @@
+import { useContext } from 'react'
 import { useState } from 'react'
+import { addNewRoomToRoomCollection } from '../../backend/FirebaseModule'
+import { ChatContext } from '../../store/ChatState'
 import styles from './AddRoomForm.module.css'
 
 const AddRoomForm = ({ hideForm }) => {
   const [title, setTitle] = useState('')
+  const { addNewRoom } = useContext(ChatContext)
 
-  const submit = e => {
+  const submit = async e => {
     e.preventDefault()
+    const room = {
+      title,
+      participants: [],
+    }
+
+    const newRoom = await addNewRoomToRoomCollection(room)
+    if (Object.keys(newRoom).length !== 0) {
+      addNewRoom({
+        ...newRoom,
+        id: newRoom.id,
+      })
+      setTitle('')
+    }
+
     hideForm()
   }
 
@@ -16,6 +34,7 @@ const AddRoomForm = ({ hideForm }) => {
         placeholder='Room Title'
         value={title}
         onChange={e => setTitle(e.target.value)}
+        required
       />
       <button className={styles.submit} type='submit'>
         Add room
