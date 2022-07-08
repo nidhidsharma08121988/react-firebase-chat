ef0e0eb4058bb9af23e732fd2885b19be66ef7cd
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-08 15:13:54 +0530

    feat: add new room * we will add create room component inside the rooms nav

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -1,24 +1,29 @@
-import styles from './ChatRoom.module.css'
-import Rooms from './Rooms'
 import { ChatContext } from '../store/ChatState'
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+
+import styles from './ChatRoom.module.css'
+import Rooms from './Rooms'
+
 import Messages from './sub_components/Messages'
 import SendMessageForm from './sub_components/SendMessageForm'
 import { CreateRoomSection } from './sub_components/CreateRoomSection'
 
-const showRooms = (
-  <div className={styles.chatRoomContainer}>
-    <div className={styles.chatRoomsNav}>
-      <Rooms />
+const ShowRooms = () => {
+  return (
+    <div className={styles.chatRoomContainer}>
+      <div className={styles.chatRoomsNav}>
+        <Rooms />
+        <CreateRoomSection />
+      </div>
+      <div className={styles.chatArea}>
+        <Messages />
+        <SendMessageForm />
+      </div>
     </div>
-    <div className={styles.chatArea}>
-      <Messages />
-      <SendMessageForm />
-    </div>
-  </div>
-)
-const showWarning = (
+  )
+}
+const ShowWarning = () => (
   <div className={styles.warningContainer}>
     <h3>Please login to access this url</h3>
     <div>
@@ -33,7 +38,7 @@ const showWarning = (
 )
 const ChatRoom = () => {
   const { isLoggedIn } = useContext(ChatContext)
-  return isLoggedIn ? showRooms : showWarning
+  return isLoggedIn ? <ShowRooms /> : <ShowWarning />
 }
 
 export default ChatRoom
