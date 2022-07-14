8f2dd39689cbb5b22855708a5d9697fe6fc00fdd
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-14 12:00:02 +0530

    refactor and style: add AddParticipantsForm and add style to it

--- src/components/sub_components/AddParticipantsForm.js
+++ src/components/sub_components/AddParticipantsForm.js
@@ -1,15 +1,17 @@
+import { useContext } from 'react'
 import styles from './AddParticipantsForm.module.css'
+import { ChatContext } from './../../store/ChatState'
 
 export const AddParticipantsForm = () => {
+  const { selectedRoom } = useContext(ChatContext)
   return (
-    <div className={styles.addParticipantsForm}>
-      <div className={styles.addParticipantsIcon}>
-        <i class='fas fa-user-plus'></i>
-      </div>
-      <div className={styles.addParticipantsInputAndSave}>
-        <input></input>
-        <button>save</button>
-        <div></div>
+    <div className={styles.addParticipantsContainer}>
+      <div className={styles.title}>{selectedRoom.title}</div>
+      <div className={styles.addParticipantsForm}>
+        <input className={styles.participantList}></input>
+        <div className={styles.addParticipantsIcon}>
+          <i className='fas fa-user-plus'></i>
+        </div>
       </div>
     </div>
   )
