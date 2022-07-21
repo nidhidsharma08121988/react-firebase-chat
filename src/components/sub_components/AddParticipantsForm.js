6d4d943abe904a048614f9014ac5825c9c52f396
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-07-21 17:19:27 +0400

    feat:implement  add participant feature

--- src/components/sub_components/AddParticipantsForm.js
+++ src/components/sub_components/AddParticipantsForm.js
@@ -1,17 +1,44 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import styles from './AddParticipantsForm.module.css'
 import { ChatContext } from './../../store/ChatState'
+import { updateRoomCollection } from '../../backend/FirebaseModule'
 
 export const AddParticipantsForm = () => {
-  const { selectedRoom } = useContext(ChatContext)
+  const { selectedRoom, updateRoom } = useContext(ChatContext)
+  const [participant, setParticipant] = useState('')
+
+  const handleComma = e => {
+    if (e.code === 'Enter') {
+      e.preventDefault() //will not submit or click other buttons
+      const participantEmail = e.target.value
+      const newParticipantsList = [
+        ...selectedRoom.participants,
+        participantEmail,
+      ]
+      const updatedProperties = {
+        participants: newParticipantsList,
+      }
+      updateRoomCollection(selectedRoom.id, updatedProperties)
+      updateRoom(selectedRoom.id, updatedProperties)
+      setParticipant('')
+    }
+  }
+
+  const handleChange = e => {
+    if (e.code !== 'Enter') setParticipant(e.target.value)
+  }
+
   return (
     <div className={styles.addParticipantsContainer}>
       <div className={styles.title}>{selectedRoom.title}</div>
       <div className={styles.addParticipantsForm}>
-        <input className={styles.participantList}></input>
-        <div className={styles.addParticipantsIcon}>
-          <i className='fas fa-user-plus'></i>
-        </div>
+        <input
+          className={styles.participantInput}
+          value={participant}
+          placeholder='Add New Participant...'
+          onChange={handleChange}
+          onKeyDown={handleComma}
+        ></input>
       </div>
     </div>
   )
